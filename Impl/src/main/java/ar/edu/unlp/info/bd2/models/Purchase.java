cb12f393ec333b10345a90263934ed4f9929a0c3
jmsolar <matias.solar@outlook.com>
2021-07-04 10:36:31 -0300

    Fix refactor

--- Impl/src/main/java/ar/edu/unlp/info/bd2/models/Purchase.java
+++ Impl/src/main/java/ar/edu/unlp/info/bd2/models/Purchase.java
@@ -24,7 +24,7 @@ public class Purchase {
 	@OneToOne(fetch = FetchType.EAGER)
 	private ProductOnSale productOnSale;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "user_id")
 	private User client;
 	
